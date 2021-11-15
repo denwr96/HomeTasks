af02694c5d4b3d330830d1cf3f5bc8c49fca8e56
Denis Lobach <denwr@inbox.lv>
2021-11-15 11:23:30 +0200

    Hometask 6 adding

---
+++ DayFinder/DayFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DayFinder
+//
+//  Created by deniss.lobacs on 14/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
